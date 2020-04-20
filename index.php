e31ca6c6482e493e355ceeb0c63e266eb8fdb888
rebelvn <61506538+rebelvn@users.noreply.github.com>
2020-04-20 23:33:02 +0700

    Done

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$reverse = new SplStack();
+$reverse->push(6);
+$reverse->push(5);
+$reverse->push(4);
+$reverse->push(3);
+$reverse->push(2);
+$reverse->push(1);
+$count = $reverse->count();
+$arr1 = [];
+for ($i = 0; $i < $count; $i++) {
+    $reverseNew = $reverse->pop();
+    array_push($arr1, $reverseNew);
+}
+var_dump($arr1);
